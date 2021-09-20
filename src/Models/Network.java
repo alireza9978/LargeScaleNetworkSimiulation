cac6b3e76849bb537160900f8c0a17c77b321cb8
Alireza Rahimi <rahimialireza.az@gmail.com>
2021-09-20 21:15:12 +0430

    create network structure

--- src/Models/Network.java
+++ src/Models/Network.java
@@ -1,5 +1,12 @@
 package Models;
 
+import Models.InfrastructureConnections.NodeConnection;
+import Models.InfrastructureConnections.ServerConnection;
+import Models.InfrastructureConnections.SwitchConnection;
+import constants.NetworkStructureUtil;
+
+import java.util.ArrayList;
+
 public class Network {
 
     private Server[] servers;
@@ -8,12 +15,49 @@ public class Network {
 
     public Network(int serverCount, int switchCount, int nodeCount) {
         initialize(serverCount, switchCount, nodeCount);
+        connectSwitches(NetworkStructureUtil.getSwitchesStructure());
+        connectNodes(NetworkStructureUtil.getNodeStructure());
+        connectServers(NetworkStructureUtil.getServerStructure());
+    }
+
+    private void connectServers(ArrayList<ServerConnection> serverConnections) {
+        for (ServerConnection connection : serverConnections) {
+            switches[connection.getSwitchId()].connect(servers[connection.getId()], connection.getPort());
+        }
+    }
+
+    private void connectNodes(ArrayList<NodeConnection> nodeConnections) {
+        int index = 0;
+        for (NodeConnection connection : nodeConnections) {
+            for (int i = index; i < index + connection.getNodeCount(); i++) {
+                nodes[i].setConnection(switches[connection.getSwitchId()]);
+            }
+        }
+    }
+
+    private void connectSwitches(ArrayList<SwitchConnection> switchConnections) {
+        for (SwitchConnection connection : switchConnections) {
+            switches[connection.getStart()].connect(switches[connection.getEnd()], connection.getStartPort());
+        }
     }
 
     private void initialize(int serverCount, int switchCount, int nodeCount) {
         servers = new Server[serverCount];
         switches = new Switch[switchCount];
         nodes = new Node[nodeCount];
+
+        for (int i = 0; i < serverCount; i++) {
+            servers[i] = new Server();
+        }
+
+        for (int i = 0; i < switchCount; i++) {
+            switches[i] = new Switch();
+        }
+
+        for (int i = 0; i < nodeCount; i++) {
+            nodes[i] = new Node();
+        }
+
     }
 
     public void simulate() {
