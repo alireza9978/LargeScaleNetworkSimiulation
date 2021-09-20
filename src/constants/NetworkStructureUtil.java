53b4bfd26215059418aefcb6bc0db69ac7d1a516
Alireza Rahimi <rahimialireza.az@gmail.com>
2021-09-20 21:16:31 +0430

    file to read csv for generating network structure

---
+++ src/constants/NetworkStructureUtil.java
@@ -0,0 +1,81 @@
+package constants;
+
+import Models.InfrastructureConnections.NodeConnection;
+import Models.InfrastructureConnections.ServerConnection;
+import Models.InfrastructureConnections.SwitchConnection;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static constants.Constants.*;
+
+public class NetworkStructureUtil {
+
+    public static List<CSVRecord> readCsv(String path) {
+        try (
+                Reader reader = Files.newBufferedReader(Paths.get(path));
+                CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT)) {
+            return csvParser.getRecords().subList(1, (int) csvParser.getRecordNumber());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static ArrayList<SwitchConnection> getSwitchesStructure() {
+        List<CSVRecord> records = readCsv("/home/alireza/projects/java/largeScaleNetworkSimiulation/src/NetworkStructureFiles/switches.csv");
+        ArrayList<SwitchConnection> switchConnections = new ArrayList<>();
+        assert records != null;
+        for (CSVRecord record : records) {
+            switchConnections.add(new SwitchConnection(Integer.parseInt(record.get(0)), Integer.parseInt(record.get(2)),
+                    Integer.parseInt(record.get(1)), Integer.parseInt(record.get(3))));
+        }
+        return switchConnections;
+    }
+
+    public static ArrayList<NodeConnection> getNodeStructure() {
+        List<CSVRecord> records = readCsv("/home/alireza/projects/java/largeScaleNetworkSimiulation/src/NetworkStructureFiles/edgesSwitches.csv");
+        assert records != null;
+
+        Random random = new Random();
+        ArrayList<NodeConnection> nodeConnections = new ArrayList<>();
+        int[] switchNodeCount = new int[EDGE_SWITCH_COUNT];
+        int maxIteration = EDGE_SWITCH_COUNT;
+        if (EDGE_SWITCH_COUNT % 2 == 1) {
+            maxIteration -= 1;
+            switchNodeCount[EDGE_SWITCH_COUNT - 1] = EDGE_SWITCH_NODE_COUNT;
+        }
+        for (int i = 0; i < maxIteration; i += 2) {
+            int temp = Math.round((random.nextFloat() - 0.5f) * 2 * NODE_COUNT_TOLERANCE);
+            switchNodeCount[i] = EDGE_SWITCH_NODE_COUNT + temp;
+            switchNodeCount[i + 1] = EDGE_SWITCH_NODE_COUNT - temp;
+        }
+
+        for (int i = 0; i < records.size(); i++) {
+            CSVRecord record = records.get(i);
+            nodeConnections.add(new NodeConnection(Integer.parseInt(record.get(0)), switchNodeCount[i]));
+        }
+        return nodeConnections;
+    }
+
+
+    public static ArrayList<ServerConnection> getServerStructure() {
+        List<CSVRecord> records = readCsv("/home/alireza/projects/java/largeScaleNetworkSimiulation/src/NetworkStructureFiles/servers.csv");
+        assert records != null;
+        ArrayList<ServerConnection> serverConnections = new ArrayList<>();
+        for (CSVRecord record : records) {
+            serverConnections.add(new ServerConnection(Integer.parseInt(record.get(0)), Integer.parseInt(record.get(1)),
+                    Integer.parseInt(record.get(2))));
+        }
+        return serverConnections;
+    }
+}
+
