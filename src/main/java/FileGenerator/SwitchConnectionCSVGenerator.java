a62b9395fda43fad0f5269bd7fd55f0254e06980
Alireza Rahimi <rahimialireza.az@gmail.com>
2021-10-25 23:04:00 +0330

    generate more csv file for large scale network specify edge switches and server location

--- src/main/java/FileGenerator/SwitchConnectionCSVGenerator.java
+++ src/main/java/FileGenerator/SwitchConnectionCSVGenerator.java
@@ -1,5 +1,6 @@
 package FileGenerator;
 
+import constants.Constants;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 
@@ -8,35 +9,55 @@ import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class SwitchConnectionCSVGenerator {
 
     public static void main(String[] args) {
-        String path = "/home/alireza/projects/java/largeScaleNetworkSimiulation/src/main/java/NetworkStructureFiles/switches_generated.csv";
+        String switchesPath = Constants.ROOT_DIR + "src/main/java/NetworkStructureFiles/switches_generated.csv";
+        String serversPath = Constants.ROOT_DIR + "src/main/java/NetworkStructureFiles/servers_generated.csv";
+        String edgeSwitchesPath = Constants.ROOT_DIR + "src/main/java/NetworkStructureFiles/edge_switches_generated.csv";
         try (
-                Writer writer = Files.newBufferedWriter(Paths.get(path));
-                CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT.withHeader("id", "port", "target"));
+                Writer switchesWriter = Files.newBufferedWriter(Paths.get(switchesPath));
+                Writer serversWriter = Files.newBufferedWriter(Paths.get(serversPath));
+                Writer edgeSwitchesWriter = Files.newBufferedWriter(Paths.get(edgeSwitchesPath));
+                CSVPrinter switchersCsvPrinter = new CSVPrinter(switchesWriter, CSVFormat.DEFAULT.withHeader("id", "port", "target", "target_port"));
+                CSVPrinter serversCsvPrinter = new CSVPrinter(serversWriter, CSVFormat.DEFAULT.withHeader("id", "switch", "port"));
+                CSVPrinter edgeSwitchesCsvPrinter = new CSVPrinter(edgeSwitchesWriter, CSVFormat.DEFAULT.withHeader("id"));
         ) {
-            Sample sample = createNetwork(5);
-            for (SampleConnection sampleConnection : sample.getConnections()) {
-                csvPrinter.printRecord(sampleConnection.getSampleIdStart(), sampleConnection.getSampleIdStartPort(), sampleConnection.getSampleIdEnd());
+            Sample sample = createNetwork(5, "");
+
+            serversCsvPrinter.printRecord(0, sample.getTopConnection().getId(), 2);
+            serversCsvPrinter.printRecord(1, sample.getRightConnection().getId(), 2);
+            serversCsvPrinter.printRecord(2, sample.getLeftConnection().getId(), 2);
+            serversCsvPrinter.flush();
+
+            for (Integer switchId : sample.getEdgeSwitches()) {
+                edgeSwitchesCsvPrinter.printRecord(switchId);
             }
-            csvPrinter.flush();
+            edgeSwitchesCsvPrinter.flush();
+
+            for (SampleConnection sampleConnection : sample.getConnections()) {
+                switchersCsvPrinter.printRecord(sampleConnection.getSampleIdStart(), sampleConnection.getSampleStartPort(),
+                        sampleConnection.getSampleIdEnd(), sampleConnection.getSampleEndPort());
+            }
+            switchersCsvPrinter.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static Sample createSample() {
-        return new Sample(new Switch(), new Switch(), new Switch());
+    private static Sample createSample(String path) {
+        return new Sample(new Switch(path + "a"), new Switch(path + "b"), new Switch(path + "c"));
     }
 
-    private static Sample createNetwork(int size) {
+    private static Sample createNetwork(int size, String path) {
         if (size <= 0) {
-            return createSample();
+            return createSample(path);
         } else {
             int tempSize = size - 1;
-            return new Sample(createNetwork(tempSize), createNetwork(tempSize), createNetwork(tempSize));
+            return new Sample(createNetwork(tempSize, path + "a"), createNetwork(tempSize, path + "b"),
+                    createNetwork(tempSize, path + "c"));
         }
     }
 
@@ -44,16 +65,25 @@ public class SwitchConnectionCSVGenerator {
 
         private static int ID;
         private final int id;
+        private final boolean edge;
 
-        public Switch() {
+        public Switch(String path) {
             id = ID;
             ID++;
+            HashSet<Character> characterHashSet = new HashSet<>();
+            for (int i = 0; i < path.length(); i++) {
+                characterHashSet.add(path.charAt(i));
+            }
+            edge = characterHashSet.size() < 3;
         }
 
         public int getId() {
             return id;
         }
 
+        public boolean isEdge() {
+            return edge;
+        }
     }
 
     private static class Sample {
@@ -130,22 +160,42 @@ public class SwitchConnectionCSVGenerator {
                         && b instanceof SwitchConnectionCSVGenerator.Switch
                         && c instanceof SwitchConnectionCSVGenerator.Switch) {
 
-                    connections.add(new SampleConnection(((Switch) a).getId(), ((Switch) b).getId(), 0));
-                    connections.add(new SampleConnection(((Switch) a).getId(), ((Switch) c).getId(), 1));
-                    connections.add(new SampleConnection(((Switch) b).getId(), ((Switch) a).getId(), 0));
-                    connections.add(new SampleConnection(((Switch) b).getId(), ((Switch) c).getId(), 1));
-                    connections.add(new SampleConnection(((Switch) c).getId(), ((Switch) a).getId(), 0));
-                    connections.add(new SampleConnection(((Switch) c).getId(), ((Switch) b).getId(), 1));
+                    connections.add(new SampleConnection(((Switch) a).getId(), ((Switch) b).getId(), 0, 0));
+                    connections.add(new SampleConnection(((Switch) a).getId(), ((Switch) c).getId(), 1, 0));
+                    connections.add(new SampleConnection(((Switch) b).getId(), ((Switch) c).getId(), 1, 1));
                 } else {
-                    connections.add(new SampleConnection(a.getRightConnection().getId(), b.getTopConnection().getId(), 2));
-                    connections.add(new SampleConnection(a.getLeftConnection().getId(), c.getTopConnection().getId(), 2));
-                    connections.add(new SampleConnection(c.getRightConnection().getId(), b.getLeftConnection().getId(), 2));
-                    connections.add(new SampleConnection(b.getTopConnection().getId(), a.getRightConnection().getId(), 2));
-                    connections.add(new SampleConnection(c.getTopConnection().getId(), a.getLeftConnection().getId(), 2));
-                    connections.add(new SampleConnection(b.getLeftConnection().getId(), c.getRightConnection().getId(), 2));
+                    connections.add(new SampleConnection(a.getRightConnection().getId(), b.getTopConnection().getId(), 2, 2));
+                    connections.add(new SampleConnection(a.getLeftConnection().getId(), c.getTopConnection().getId(), 2, 2));
+                    connections.add(new SampleConnection(c.getRightConnection().getId(), b.getLeftConnection().getId(), 2, 2));
                 }
             }
             return connections;
+        }
+
+
+        public void getInnerEdge(Sample sample, ArrayList<Integer> edgeSwitchesId) {
+            if (sample instanceof Switch) {
+                if (((Switch) sample).isEdge()) {
+                    edgeSwitchesId.add(((Switch) sample).getId());
+                }
+            } else {
+                getInnerEdge(sample.getA(), edgeSwitchesId);
+                getInnerEdge(sample.getB(), edgeSwitchesId);
+                getInnerEdge(sample.getC(), edgeSwitchesId);
+            }
+        }
+
+        public ArrayList<Integer> getEdgeSwitches() {
+            ArrayList<Integer> edgeSwitchesId = new ArrayList<>();
+            getInnerEdge(getA(), edgeSwitchesId);
+            getInnerEdge(getB(), edgeSwitchesId);
+            getInnerEdge(getC(), edgeSwitchesId);
+            ArrayList<Integer> serverConnectionSwitches = new ArrayList<>();
+            serverConnectionSwitches.add(getRightConnection().getId());
+            serverConnectionSwitches.add(getLeftConnection().getId());
+            serverConnectionSwitches.add(getTopConnection().getId());
+            edgeSwitchesId.removeAll(serverConnectionSwitches);
+            return edgeSwitchesId;
         }
 
     }
@@ -154,12 +204,14 @@ public class SwitchConnectionCSVGenerator {
 
         private final int sampleIdStart;
         private final int sampleIdEnd;
-        private final int sampleIdStartPort;
+        private final int sampleStartPort;
+        private final int sampleEndPort;
 
-        public SampleConnection(int sampleIdStart, int sampleIdEnd, int sampleIdStartPort) {
+        public SampleConnection(int sampleIdStart, int sampleIdEnd, int sampleStartPort, int sampleEndPort) {
             this.sampleIdStart = sampleIdStart;
             this.sampleIdEnd = sampleIdEnd;
-            this.sampleIdStartPort = sampleIdStartPort;
+            this.sampleStartPort = sampleStartPort;
+            this.sampleEndPort = sampleEndPort;
         }
 
         public int getSampleIdStart() {
@@ -170,8 +222,12 @@ public class SwitchConnectionCSVGenerator {
             return sampleIdEnd;
         }
 
-        public int getSampleIdStartPort() {
-            return sampleIdStartPort;
+        public int getSampleStartPort() {
+            return sampleStartPort;
+        }
+
+        public int getSampleEndPort() {
+            return sampleEndPort;
         }
     }
 
