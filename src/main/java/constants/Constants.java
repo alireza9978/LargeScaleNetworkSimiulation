dfaaa3f599ce10d240b3d444b58bd3c4bf0922fd
Alireza Rahimi <rahimialireza.az@gmail.com>
2021-10-26 13:06:34 +0330

    add two new node type

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -2,19 +2,23 @@ package constants;
 
 public class Constants {
 
-    public static final int MAX_SERVER_COUNT = 50;
+    public static final int MAX_SERVER_COUNT = 5;
+    public static final int MAX_VM_IN_SINGLE_SERVER_COUNT = 10;
     public static final int MAX_SWITCH_COUNT = 1000;
-    public static final int MAX_NODE_COUNT = 60;
 
+    public static final int MAX_NODE_COUNT = 105_000;
+    // we have 168 edge node maxNodeCount must be dividable by 168
+    public static final int MINIMUM_ACTIVE_NODE_COUNT = 4_200;
+    // maxNodeCount - minimumActiveNodeCount must be dividable by 24*12
 
-    public static final int MINIMUM_ACTIVE_NODE_COUNT = 10;
     public static final int EDGE_SWITCH_COUNT = 3;
     public static int EDGE_SWITCH_NODE_COUNT = MAX_NODE_COUNT / EDGE_SWITCH_COUNT;
     public static int NODE_COUNT_TOLERANCE = EDGE_SWITCH_NODE_COUNT;
 
     public static final int SWITCH_MAX_CONNECTION_COUNT = 32;
     public static final int SERVER_MAX_VM_COUNT = 10;
-    public static final int SERVER_MAX_PROCESSING_PACKET_IN_HOUR = 100000;
+    public static final int SERVER_MAX_PROCESSING_PACKET_IN_HOUR = 120000;
+    public static final int SERVER_MAX_PROCESSING_PACKET_IN_SECOND = SERVER_MAX_PROCESSING_PACKET_IN_HOUR / 60;
 
     // packets that each vm can process in a single cycle
     public static final int MAX_VM_PACKET_COUNT_PROCESS_SPEED = 10000;
@@ -25,15 +29,17 @@ public class Constants {
     public static final int CLASS_ONE_SIZE = 1000;
     public static final int CLASS_TWO_SIZE = 2000;
     public static final int CLASS_THREE_SIZE = 4000;
+    public static final int CLASS_FOUR_SIZE = 4000;
 
     // classes packets count in second
-    public static final int CLASS_ONE_COUNT = 10;
-    public static final int CLASS_TWO_COUNT = 7;
-    public static final int CLASS_THREE_COUNT = 5;
+    public static final int CLASS_ONE_COUNT = 1;
+    public static final int CLASS_TWO_COUNT = 2;
+    public static final int CLASS_THREE_COUNT = 4;
+    public static final int CLASS_FOUR_COUNT = 1;
 
     // link speed 10Mb/s
     public static final long LINK_SPEED_IN_SECOND = 10_000_000L;
-    public static final int GCD_CLASSES_PACKET_SIZE = gcd(gcd(CLASS_ONE_SIZE, CLASS_TWO_SIZE), CLASS_THREE_SIZE);
+    public static final int GCD_CLASSES_PACKET_SIZE = gcd(gcd(gcd(CLASS_ONE_SIZE, CLASS_TWO_SIZE), CLASS_THREE_SIZE), CLASS_FOUR_SIZE);
     public static final long CLOCK_IN_SECOND = LINK_SPEED_IN_SECOND / GCD_CLASSES_PACKET_SIZE;
     public static final long TOTAL_CLOCK_COUNT = 24L * 60L * 60L * CLOCK_IN_SECOND;
     public static final long SMALL_TOTAL_CLOCK_COUNT = 60L * 60L * CLOCK_IN_SECOND;
@@ -46,6 +52,7 @@ public class Constants {
     public static final int CLASS_ONE_CYCLE = (int) CLOCK_IN_SECOND / CLASS_ONE_COUNT;
     public static final int CLASS_TWO_CYCLE = (int) CLOCK_IN_SECOND / CLASS_TWO_COUNT;
     public static final int CLASS_THREE_CYCLE = (int) CLOCK_IN_SECOND / CLASS_THREE_COUNT;
+    public static final int CLASS_FOUR_CYCLE = (int) CLOCK_IN_SECOND / CLASS_FOUR_COUNT;
 
     public static final String FIGURE_DIR = "src/main/resources/charts/";
     public static final String GRAPH_DIR = "src/main/resources/graphs/";
--- src/main/java/constants/NodeType.java
+++ src/main/java/constants/NodeType.java
@@ -5,12 +5,14 @@ import java.util.Random;
 public enum NodeType {
 
     health,
-    traffic;
+    traffic,
+    securityCamera,
+    powerConsumption;
 
     private static final Random random = new Random();
 
     public static int getCount() {
-        return 2;
+        return 4;
     }
 
     public static NodeType getInstance(String id) {
@@ -22,54 +24,72 @@ public enum NodeType {
             case 1: {
                 return traffic;
             }
+            case 2: {
+                return securityCamera;
+            }
+            case 3: {
+                return powerConsumption;
+            }
         }
         return null;
     }
 
     public static NodeType getInstanceRandom() {
-        int tempId = Math.round(random.nextFloat() * 10);
-        if (tempId >= 4) {
-            tempId = 1;
-        } else {
-            tempId = 0;
+        int tempId = Math.round(random.nextFloat() * 100);
+//        0~10 powerConsumption
+//        10~30 securityCamera
+//        30~60 traffic
+//        60~100 health
+        if (tempId < 10) {
+            return powerConsumption;
+        } else if (tempId < 30) {
+            return securityCamera;
+        } else if (tempId < 60) {
+            return traffic;
+        } else if (tempId < 100) {
+            return health;
         }
-        switch (tempId) {
-            case 0: {
-                return health;
-            }
-            case 1: {
-                return traffic;
-            }
-        }
-        return null;
+        return health;
     }
 
-    public int getPeriod(){
-        switch (this){
+    public int getPeriod() {
+        switch (this) {
             case health:
                 return Constants.CLASS_ONE_CYCLE;
             case traffic:
                 return Constants.CLASS_TWO_CYCLE;
+            case securityCamera:
+                return Constants.CLASS_THREE_CYCLE;
+            case powerConsumption:
+                return Constants.CLASS_FOUR_CYCLE;
         }
         return -1;
     }
 
-    public int getSize(){
-        switch (this){
+    public int getSize() {
+        switch (this) {
             case health:
                 return Constants.CLASS_ONE_SIZE;
             case traffic:
                 return Constants.CLASS_TWO_SIZE;
+            case securityCamera:
+                return Constants.CLASS_THREE_SIZE;
+            case powerConsumption:
+                return Constants.CLASS_FOUR_SIZE;
         }
         return -1;
     }
 
-    public int toInt(){
-        switch (this){
+    public int toInt() {
+        switch (this) {
             case health:
                 return 0;
             case traffic:
                 return 1;
+            case securityCamera:
+                return 2;
+            case powerConsumption:
+                return 3;
         }
         return -1;
     }
