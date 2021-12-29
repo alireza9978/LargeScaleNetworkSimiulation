41c0002a908b6aad92a35795509bf621eebe984b
Alireza Rahimi <rahimialireza.az@gmail.com>
2021-12-29 17:21:42 +0330

    fix some active nodes bug

--- src/main/java/FileGenerator/NodeStateCSVGeneratorGaussian.java
+++ src/main/java/FileGenerator/NodeStateCSVGeneratorGaussian.java
@@ -4,6 +4,9 @@ import constants.Constants;
 import constants.Gaussian;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
+import org.knowm.xchart.BitmapEncoder;
+import org.knowm.xchart.QuickChart;
+import org.knowm.xchart.XYChart;
 
 import java.io.IOException;
 import java.io.Writer;
@@ -11,9 +14,13 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
-import static constants.Constants.MINIMUM_ACTIVE_NODE_COUNT;
+import static constants.Constants.*;
 
 public class NodeStateCSVGeneratorGaussian {
+
+    private final ArrayList<Long> timeArray = new ArrayList<>();
+    private final ArrayList<Long> activeNodeCount = new ArrayList<>();
+    private final ArrayList<Long> stepArray = new ArrayList<>();
 
     public void create() {
         String path = Constants.ROOT_DIR + "src/main/java/NetworkStructureFiles/activeNode.csv";
@@ -23,31 +30,28 @@ public class NodeStateCSVGeneratorGaussian {
         ) {
             int minActiveCount = MINIMUM_ACTIVE_NODE_COUNT;
             int smallPeak = Constants.MAX_NODE_COUNT / 2 - minActiveCount;
-            int bigPeak = Constants.MAX_NODE_COUNT - minActiveCount;
-            long totalStep = 360;
+            int bigPeak = (int) (Constants.MAX_NODE_COUNT * 0.9) - minActiveCount;
+            long totalStep = NODE_ACTIVATION_STEPS_COUNT;
             long timeStep = Constants.TOTAL_CLOCK_COUNT / totalStep;
             long time = 0;
 
-            ArrayList<Long> timeArray = new ArrayList<>();
-            ArrayList<Long> activeNodeCount = new ArrayList<>();
-            ArrayList<Long> stepArray = new ArrayList<>();
             Gaussian small = new Gaussian(12, 110);
             Gaussian big = new Gaussian(9, 270);
             Long tempActiveNodeCount = null;
             for (int i = 0; i < totalStep; i++) {
                 timeArray.add(time);
-                if (tempActiveNodeCount == null){
+                if (tempActiveNodeCount == null) {
                     tempActiveNodeCount = Math.round(small.getY(i) * smallPeak + big.getY(i) * bigPeak + minActiveCount);
                     stepArray.add(tempActiveNodeCount);
-                }else{
+                } else {
                     long temp = Math.round(small.getY(i) * smallPeak + big.getY(i) * bigPeak + minActiveCount);
                     stepArray.add(temp - tempActiveNodeCount);
                     tempActiveNodeCount = temp;
                 }
                 activeNodeCount.add(tempActiveNodeCount);
-                time+=timeStep;
+                time += timeStep;
             }
-            for (int i = 0; i < totalStep; i++){
+            for (int i = 0; i < totalStep; i++) {
                 csvPrinter.printRecord(timeArray.get(i), activeNodeCount.get(i), stepArray.get(i));
             }
             csvPrinter.flush();
@@ -56,9 +60,25 @@ public class NodeStateCSVGeneratorGaussian {
         }
     }
 
+    public ArrayList<Long> getTimeArray() {
+        return timeArray;
+    }
+
+    public ArrayList<Long> getActiveNodeCount() {
+        return activeNodeCount;
+    }
+
     public static void main(String[] args) {
         NodeStateCSVGeneratorGaussian nodeStateCSVGenerator = new NodeStateCSVGeneratorGaussian();
         nodeStateCSVGenerator.create();
+        XYChart inputPacketChart = QuickChart.getChart("Node State Gaussian", "Time", "Active Node Count",
+                "active node count", nodeStateCSVGenerator.getTimeArray(), nodeStateCSVGenerator.getActiveNodeCount());
+        try {
+            BitmapEncoder.saveBitmapWithDPI(inputPacketChart, ROOT_DIR + "src/main/resources/generated/gaussian",
+                    BitmapEncoder.BitmapFormat.PNG, 500);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
